91af058df4be58d5a8b28879a928704160de1481
Richard Hanolt <rhanolt@gmail.com>
2020-07-07 00:57:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,6 +4,7 @@ class Dog
   attr_reader :name
 
   @@all = []
+
   def initialize(name)
     @name = name
     @@all << self
@@ -19,7 +20,7 @@ class Dog
 
   def self.print_all
     @@all.each do |dog|
-      puts dog
+      puts dog.name
     end
   end
 
